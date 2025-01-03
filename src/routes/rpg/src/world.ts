6e2d2d2d99d4099d770bd17d91ce4c4ddbe1a35f
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-03 11:25:59 -0300

    feat(rpg): avoid creating multiple trees in the same place

--- src/routes/rpg/src/world.ts
+++ src/routes/rpg/src/world.ts
@@ -4,7 +4,7 @@ import type { Element } from "./element";
 import { Tree } from "./tree";
 
 export class World extends THREE.Mesh {
-  #elements = new THREE.Group();
+  #map = new Map<THREE.Vector3, Element>();
   #width;
   #height;
   constructor(width: number, height: number, tree_count: number) {
@@ -14,7 +14,9 @@ export class World extends THREE.Mesh {
     this.make_terrain();
     this.make_grid();
     this.make_trees(tree_count);
-    this.add(this.#elements);
+    for (const element of this.#map.values()) {
+      this.add(element);
+    }
   }
   normalize_position(element: Element) {
     element.position.set(
@@ -23,9 +25,47 @@ export class World extends THREE.Mesh {
       element.position.z - this.#height / 2 + 0.5,
     );
   }
+  get_free_z_cells() {
+    const all_cells = Array.from({ length: this.#height }, (_, i) => {
+      return i;
+    });
+    const free_cells: number[] = [];
+    for (let z = 0; z < all_cells.length; z++) {
+      if (
+        this.#map.keys().some((coordinate) => {
+          return coordinate.z === z;
+        })
+      ) {
+        continue;
+      }
+      free_cells.push(z);
+    }
+    return free_cells;
+  }
+  get_free_x_cells() {
+    const all_cells = Array.from({ length: this.#width }, (_, i) => {
+      return i;
+    });
+    const free_cells: number[] = [];
+    for (let x = 0; x < all_cells.length; x++) {
+      if (
+        this.#map.keys().some((coordinate) => {
+          return coordinate.x === x;
+        })
+      ) {
+        continue;
+      }
+      free_cells.push(x);
+    }
+    return free_cells;
+  }
+  get_random_number_from_list(list: number[]) {
+    const index = Math.floor(Math.random() * list.length);
+    return list[index];
+  }
   compose_random_position(element: Element) {
-    const x = Math.floor(Math.random() * this.#width);
-    const z = Math.floor(Math.random() * this.#height);
+    const x = this.get_random_number_from_list(this.get_free_x_cells());
+    const z = this.get_random_number_from_list(this.get_free_z_cells());
     element.position.set(x, element.position.y, z);
   }
   make_trees(count: number) {
@@ -39,7 +79,7 @@ export class World extends THREE.Mesh {
         tree.position.z,
       );
       this.normalize_position(tree);
-      this.#elements.add(tree);
+      this.#map.set(tree.position, tree);
     }
   }
   make_grid() {
